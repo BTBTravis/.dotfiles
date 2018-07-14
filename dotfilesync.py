c3e42764ba8a0fd0156febda256df9a2f971cefc
Travis Shears <btbtravis@gmail.com>
2018-07-14 10:30:07 +0200

    Update script to MVP and add .vimrc

--- dotfilesync.py
+++ dotfilesync.py
@@ -1,18 +1,30 @@
 #!/usr/bin/env python
-# This is my first script.
+# inspired by http://blog.smalleycreative.com/tutorials/using-git-and-github-to-manage-your-dotfiles/
 import subprocess
 import csv
 import sys
 
+# global vars
+selfpath = '~/dotfiles' # dotfiles directory
+backup = '~/dotfiles_old' # old dotfiles backup directory
+
+
+# get files names
 f = open('dotfilemappings.csv', 'rb')
 reader = csv.reader(f)
+# ex: .test,~/.test
 
-for r in reader:
-    print r
+# make backup folder
+print 'Creating ' + backup + ' for backup of any existing dotfiles'
+subprocess.call('mkdir -p ' + backup, shell=True)
+print "...done"
+
+# move files into backup and create syslinks in there place
+for fileinfo in reader:
+    print 'Moving any existing dotfiles into ' + backup
+    subprocess.call('mv ' + fileinfo[1] + ' ' + backup, shell=True)
+    print "Creating symlink to $file in home directory."
+    subprocess.call('ln -s ' + selfpath + '/' + fileinfo[0] + ' ' + fileinfo[1], shell=True)
+print 'done'
 
 f.close()
-
-
-
-
-subprocess.call("ls -l", shell=True)
