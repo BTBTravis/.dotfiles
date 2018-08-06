cc036d4eb234febd18ec722970cdb90e969a28cb
Travis Shears <btbtravis@gmail.com>
2018-08-06 21:04:23 +0200

    Update write.sh script

--- write.sh
+++ write.sh
@@ -1,11 +1,26 @@
 #!/usr/bin/env bash
+LINUX_FOLDER="linux_configs"
 
+case "$(uname -s)" in
+    Linux*)     OS=Linux;;
+    Darwin*)    OS=Mac;;
+    *)          OS="UNKNOWN"
+esac
+
+if [ $OS != "Linux" ] &&  [ $OS != "Mac" ]; then
+    exit 1
+fi
 # make backup folder
 echo "Making backup folder ~/dotfiles_old"
 mkdir -p ~/dotfiles_old
 ID=$(date +%Y%m%d_%H%M%S)
+if [ $OS == "Linux" ]; then
+    echo "Creating temp folder for linux configs"
+    mkdir $LINUX_FOLDER
+fi
+
 while read -r; do
-    LOCAL_FILE=$(echo $REPLY | gawk -F, '{print $1}')
+    LOCAL_FILE=$(echo $REPLY | gawk -F, '{print $1}' | (echo -n "config_files/" && cat))
     REMOTE_FILE=$(echo $REPLY | gawk -F, '{print $2}')
     REMOTE_FILE_NAME=$(echo $REMOTE_FILE | ag -o '[^\/]+$')
     echo "Moving any existing file: "$REMOTE_FILE "into ~/dotfiles_old"
