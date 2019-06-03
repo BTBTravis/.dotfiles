cbd95990f2b323902c7ca156d95502792a66ca8c
Travis Shears <btbtravis@gmail.com>
2019-06-03 19:25:44 +0200

    Init sort_tasks.pl

---
+++ mac/vim_scripts/sort_tasks.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+my @done_tasks;
+my @todo_tasks;
+my $todo_type = "todo";
+my $done_type = "done";
+my $last_task_type = "none";
+
+while (<>) {
+    if (/^- \[X/) {
+        push @done_tasks, $_;
+        $last_task_type = $done_type;
+    } elsif (/^- \[/) {
+        push @todo_tasks, $_;
+        $last_task_type = $todo_type;
+    } elsif ($last_task_type eq $todo_type && @todo_tasks > 0) {
+        $todo_tasks[@todo_tasks - 1] = $todo_tasks[@todo_tasks - 1] . $_;
+    } elsif ($last_task_type eq $done_type && @done_tasks > 0) {
+        $done_tasks[@done_tasks - 1] = $done_tasks[@done_tasks - 1] . $_;
+    }
+}
+
+foreach (@todo_tasks) {
+    print;
+}
+
+foreach (@done_tasks) {
+    print;
+}
